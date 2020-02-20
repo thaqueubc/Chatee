2b4018feeb5c5e14168727cdab2282316d0ff663
thaque <reya.ruet@gmail.com>
2020-02-20 15:09:30 -0800

    Recycler view to display chat messages

--- app/src/main/java/com/thaque/chatee/ChatActivity.kt
+++ app/src/main/java/com/thaque/chatee/ChatActivity.kt
@@ -1,38 +1,53 @@
 package com.thaque.chatee
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.item_message.view.*
 
 class ChatActivity: AppCompatActivity() {
-    private lateinit var messagesDB: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_chat)
-        messagesDB = FirebaseDatabase.getInstance().getReference("Messages")
-        send_message_button.setOnClickListener {
-            val sender = FirebaseAuth.getInstance().currentUser?.email
-            val message = message_input.text.toString()
 
-            if (sender != null) {
-                saveMessage(sender, message)
-            }
-        }
-    }
+        message_list.layoutManager = LinearLayoutManager(this)
 
-    private fun saveMessage(sender: String, messageBody: String) {
-        val key = messagesDB.push().key
-        key ?: return
+        val messages = listOf(
+            Message("someguy@example.com", "Oh hai! Blah blah blah blah blahblahblah."),
+            Message("someotherguy@example.com", "Yaya, blee blee bleeee.")
+        )
 
-        val message = Message(sender, messageBody)
-
-        messagesDB.child(key).setValue(message)
+        message_list.adapter = MessagesAdapter(messages, this)
     }
 }
-data class Message(
-    var sender: String = "",
-    var messageBody: String = "")
+
+private class MessagesAdapter(private val messages: List<Message>, val context: Context): RecyclerView.Adapter<MessagesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessagesViewHolder {
+        return MessagesViewHolder(LayoutInflater.from(context).inflate(R.layout.item_message, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return messages.count()
+    }
+
+    override fun onBindViewHolder(holder: MessagesViewHolder, position: Int) {
+        val message = messages[position]
+
+        holder.itemView.sender_label.text = message.sender
+        holder.itemView.message_body_label.text = message.messageBody
+    }
+}
+
+class MessagesViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+data class Message(val sender: String, val messageBody: String)
