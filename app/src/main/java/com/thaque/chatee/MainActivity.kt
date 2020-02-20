d74576de865f48ead6262d1f2dfe8593753db2a0
thaque <reya.ruet@gmail.com>
2020-02-20 12:04:18 -0800

    Refactor Form as Login and Registration

--- app/src/main/java/com/thaque/chatee/MainActivity.kt
+++ app/src/main/java/com/thaque/chatee/MainActivity.kt
@@ -3,8 +3,11 @@ package com.thaque.chatee
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.activity_auth.auth_button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,13 +15,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val database = FirebaseDatabase.getInstance().reference
-       // val myRef = database.getReference("message")
-
-        database.setValue("Hello World")
-
         login_button.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(AuthActivity.newIntent(AuthMode.Login, this))
+        }
+
+        register_button.setOnClickListener {
+            startActivity(AuthActivity.newIntent(AuthMode.Register, this))
         }
     }
+}
+
+sealed class AuthMode() : Parcelable {
+    @Parcelize
+    object Login : AuthMode()
+
+    @Parcelize
+    object Register : AuthMode()
 }
